48354fd7867fa1536af2735313a0cdbe3b096caa
Sam Komarov <sam@skomarov.com>
2022-10-18 18:14:37 +0300

    add new methods to interface.py

--- backend/interface.py
+++ backend/interface.py
@@ -2,4 +2,15 @@ import abc
 
 
 class PollService(abc.ABC):
-    pass
+    @abc.abstractmethod
+    def get_all_users(self) -> List[str]:
+        pass
+    @abc.abstractmethod
+    def add_user(self, user: str) -> bool:
+        pass
+    @abc.abstractmethod
+    def assign_admin(self):
+        pass
+    @abc.abstractmethod
+    def respond(self, question_id: str, answer: str, from_user: str) -> bool:
+        pass
