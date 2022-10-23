f7b3400472f65a5cf6e0e660b9ee06d490bbe8b1
Sam Komarov <sam@skomarov.com>
2022-10-23 16:44:02 +0300

    change texts.py

--- lib/frontend/design/texts.py
+++ lib/frontend/design/texts.py
@@ -14,6 +14,7 @@ class Texts:
 """
 Список команд: 
 /admin_login <пароль> - получить права админа
+Команды для админов: 
 /create_poll - создание нового опроса 
 /send_poll - рассылка опроса 
 /stats - получение статистики по опросу 
@@ -26,8 +27,8 @@ class Texts:
     NO_STATS = "Для этого опроса пока нет статистики."
     NO_POLL = "Опроса с таким id не найдено."
     ENTER_TITLE = "Введите название для нового опроса: "
-    CREATION_CANCELLED = "Создание опроса отменено"
-    POLL_CREATED = "Опрос успешно создан"
+    CREATION_CANCELLED = "Создание опроса отменено."
+    POLL_CREATED = "Опрос успешно создан."
     STOP_ADDING = "Закончить добавление"
     CANCEL = "Отменить"
     PARTICIPATION_THANKS = "Спасибо за участие в опросе"
@@ -37,16 +38,22 @@ class Texts:
     def STATS(poll: Poll, stats: PollStats) -> str:
         text = f"Статистика для опроса \"{poll.title}\"\n"
         for i, qstats in enumerate(stats.question_stats):
-            text += f"{poll.questions[i].text}: Yes {qstats.yes}, No {qstats.no}, Idk {qstats.idk}\n"
+            text += \
+f"""
+{poll.questions[i].text}:
+    "Да": {qstats.yes}
+    "No": {qstats.no}
+    "Не знаю": {qstats.idk}\n
+"""
         return text
     def INVITATIONS_REPORT(invites: int) -> str:
         return f"Приглашение поучавствовать в опросе было отправлено в {invites} чат" + "ов" if invites % 10 > 1 else ""
     def ENTER_QUESTION(index: int) -> str:
         return f"Пожалуйста, введите название вопроса №{index}"
     def POLL_INVITE(poll: Poll) -> str:
-        return "Пожалуйста, поучавствуйте в опросе: " + poll.title
+        return f"Пожалуйста, поучавствуйте в опросе: \"{poll.title}\""
     def YOUR_ANSWER(answer: Answer) -> str:
-        return '\nВаш ответ: ' + Texts.ANSWER(answer)
+        return f'\nВаш ответ: {Texts.ANSWER(answer)}'
     def ANSWER(a: Answer) -> str:
         if a == Answer.YES:
             return "Да"
